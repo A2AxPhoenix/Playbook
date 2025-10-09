503f9bc51eab504eeebaa24e3dcf3bd433de0ad9
Justis Mencarelli <79618505+A2AxPhoenix@users.noreply.github.com>
2025-10-09 08:29:57 -0700

    Fixed some issues in League.h and added functionality to League (#1)

--- include/League.h
+++ include/League.h
@@ -6,9 +6,40 @@
 #include <cstdint>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 class League {
 private:
+  static constexpr uint8_t WEEKS_IN_SEASON = 18;
+  struct Matchup {
+    uint64_t id = 0;
+    Manager *teamA = nullptr;
+    Manager *teamB = nullptr;
+    bool isConcluded = false;
+    double teamAPts = 0.;
+    double teamBPts = 0.;
+  };
+  std::string leagueName;
+  Manager *commissioner;
+  uint8_t currentWeek;
+  Scoring_Rules ruleset;
+  std::unordered_map<uint64_t, Manager *> managers;
+  std::unordered_map<uint64_t, Matchup> managerSchedule;
+  std::vector<std::vector<Matchup>> schedule{WEEKS_IN_SEASON + 1};
+
+public:
+  League() = delete;
+  League(std::string newLeagueName, Manager *newCommissioner,
+         uint8_t startingWeek);
+  std::string get_leagueName() const;
+  Manager *get_commissioner() const;
+  uint8_t get_currentWeek() const;
+  const std::unordered_map<uint64_t, Manager *> &get_managers() const;
+  const std::vector<std::vector<Matchup>> &
+  get_schedule(uint64_t managerID) const;
+  std::unordered_set<Player> get_manager_roster(uint64_t managerID) const;
+  std::unordered_map<uint64_t, Matchup> get_managers_schedule() const;
+  // TODO: Give permissions for the Commissioner to modify more of the League
 };
 #endif
